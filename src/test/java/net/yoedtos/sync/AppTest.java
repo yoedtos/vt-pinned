517c398c44a302e44f038f5bca827d11941907ee
Edson Yoshimaru <yoedtos@hotmail.com>
2025-04-12 16:55:58 +0900

    test: simple version - CountDownLatch

--- src/test/java/net/yoedtos/sync/AppTest.java
+++ src/test/java/net/yoedtos/sync/AppTest.java
@@ -1,20 +1,35 @@
 package net.yoedtos.sync;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+import net.yoedtos.sync.api.Type;
+import net.yoedtos.sync.core.NoVirtual;
+import net.yoedtos.sync.core.Virtual;
+
+class AppTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
 
 	@Test
-	public void shouldAnswerWithTrue() {
-		LOGGER.debug("App Test");
-		assertTrue(true);
+	void testVirtualWithTwentThread() {
+		LOGGER.info("Virtual Test");
+		assertThatCode(() -> {
+			new Virtual(20).execute(Type.EXPLICIT);
+			new Virtual(20).execute(Type.IMPLICIT);
+		})
+		.doesNotThrowAnyException();
+	}
+
+	@Test
+	void testNoVirtualWithTwentThread() {
+		LOGGER.info("NoVirtual Test");
+		assertThatCode(() -> {
+			new NoVirtual(20).execute(Type.EXPLICIT);
+			new NoVirtual(20).execute(Type.IMPLICIT);
+		})
+		.doesNotThrowAnyException();
 	}
 }
